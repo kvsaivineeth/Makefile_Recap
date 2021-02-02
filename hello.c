e7c1b342d405a89b6d11daa4569adf8974322547
kvsaivineeth <skatasanivenkata@wpi.edu>
2021-02-01 22:37:12 -0500

    Added new files

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+#include "hello.h"
+
+int main(){
+
+    int s = 3;
+    int s1 = pow(3,2);
+    printf("%s\n", HELLO);
+    printf("Square of %d : %d\n", s, s1);
+    return 0;
+}
